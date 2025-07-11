05f0f69ddfdc5ca956d7352ea6ee509e23d8e29d
a <33055634+angeandria@users.noreply.github.com>
2025-07-11 06:28:14 +0200

    update

---
+++ _tmp-dev/themes/stack/inc/content-post-categories.php
@@ -0,0 +1,24 @@
+<?php 
+	$tax_query = $wp_query->get( 'tax_query' );
+	if( is_array( $tax_query) && !( 'category' == $tax_query[0]['taxonomy'] ) ){
+		return false;
+	}
+?>
+
+<span><?php esc_html_e('Category:', 'stack'); ?></span>
+<div class="masonry-filter-holder masonry-filter-holder-post">
+	<div class="masonry__filters blog-filters">
+		<ul>
+			<?php 
+				if( $tax_query ){
+					$idObj = get_category_by_slug($tax_query[0]['terms']);
+					echo '<li class="active js-no-action">'. $idObj->name .'</li>';
+					wp_list_categories('child_of='. $idObj->term_id .'&title_li=');
+				} else {
+					echo '<li class="active js-no-action">'. esc_html__('All Categories', 'stack') .'</li>';
+					wp_list_categories('title_li='); 
+				}
+			?>
+		</ul>
+	</div>
+</div>
